5aeaeb2b98e863e8174dfa12030f0140f21301f2
Megan Levin <mlevin@mit.edu>
2017-06-15 19:36:28 -0400

    Delete nav.js

--- nav.js
+++
@@ -1,8 +0,0 @@
-document.write('\
-<div class="links">\
-      <a href="index.html">What is Senior House?</a>\
-      <a href="admin.html">Admin Transgressions</a>\
-      <a href="demands.html">Demands</a>\
-      <a href="support.html">Support Us</a>\
-      <a href="events.html">Upcoming Events</a>\
-</div>\');
