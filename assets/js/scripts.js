e4c4809960bd581a881986c479324bba3f010750
Joseph Bergeron <jbergero@mit.edu>
2017-06-16 00:02:58 -0700

    Revert "Revert "way more mobile-friendly, still more to do though""
    This reverts commit 51feb427a32b361b386a8b40affa9cca8cac2542.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,10 +1,29 @@
-$( document ).ready(function() {      
-    var isMobile = window.matchMedia("only screen and (max-width: 700px)");
 
-    if (isMobile.matches) {
-      //Conditional script here
-      console.log("mobile");
-    } else {
-      console.log("not mobile");
-    }
- });
+
+$( document ).ready(function() {
+
+  
+  var isMobile = window.matchMedia("only screen and (max-width: 700px)");
+  console.log(isMobile.matches, jQuery.browser.mobile);
+ 
+  
+  if (isMobile.matches | jQuery.browser.mobile) {
+    //Conditional script here
+    console.log("mobile");
+    $( "header").css({height: "50%"});
+    $( "header .container").css({position: "relative", width: "100%"});
+    $( "header .container").height("100%");
+    $( ".container h1" ).css({width: "100%", height: "100%", position: "absolute", top: 0, right: 0, "font-size": "10em",  background: "rgba(0,0,0,.7)", margin: 0});
+    $( ".container h1 span ").css({background: "transparent"});
+    $( ".container h1 span" ).css({"margin-top": $( ".container h1 ").height()/2 - $(".container h1 span").height()/2});
+
+    $( "nav .links .button ").css({width: "100%", height: "10vh", "line-height": "10vh"});
+    $( "nav .links .button a").css({"font-size": "xx-large"});
+    $( ".content" ).css({"font-size": "xx-large"});
+  } else {
+    console.log("not mobile");
+  }
+
+});
+
+
